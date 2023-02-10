8bb7611b079a79cfd8723de16108b3493c8197ad
nicsylvia <nicsylvia15f@gmail.com>
2023-02-09 18:39:32 -0800

    saved part of sidebar comp

--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -5,7 +5,8 @@ const Sidebar = () => {
   return (
     <div>
         <Container>
-
+            <Menu></Menu>
+            <Menu2></Menu2>
         </Container>
     </div>
   )
@@ -15,7 +16,21 @@ export default Sidebar;
 
 const Container = styled.div`
     width: 300px;
-    background-color: red;
+    background-color: black;
     height: calc(100vh - 55px);
     box-shadow: rgba(0, 0, 0, 0.05) 0px 1px 2px 0px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-direction: column;
+`;
+const Menu = styled.div`
+    width: 100%;
+    background-color: red;
+    padding: 10px 0px 10px 0px;
+`;
+const Menu2 = styled.div`
+    width: 100%;
+    background-color: orange;
+    padding: 10px 0px 10px 0px;
 `;
