f991e25518a5ead733cdf6d4b2dd03986881cc32
nicsylvia <nicsylvia15f@gmail.com>
2023-02-09 20:03:39 -0800

    saved complete sidebar

--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -7,12 +7,59 @@ import { BsCalendar4Week, BsPeople } from "react-icons/bs";
 import { FaRegUser } from "react-icons/fa";
 import { FiHome } from "react-icons/fi";
 import { TiTickOutline } from "react-icons/ti";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const Sidebar = () => {
   return (
     <div>
         <Container>
-            <Menu></Menu>
+            <Menu>
+                <Div1>
+                    <Pic>
+                        <RxHamburgerMenu />
+                    </Pic>
+                </Div1>
+                <Div>
+                    <Pic>
+                        <MdOutlineWbSunny />
+                    </Pic>
+                    <Tittle>
+                        <span>My Day</span>
+                    </Tittle>
+                </Div>
+                <Div>
+                    <Pic>
+                        <AiOutlineStar />
+                    </Pic>
+                    <Tittle>
+                        Important
+                    </Tittle>
+                </Div>
+                <Div>
+                    <Pic>
+                        <BiCalendar />
+                    </Pic>
+                    <Tittle>
+                        Planned
+                    </Tittle>
+                </Div>
+                <Div>
+                    <Pic>
+                        <FaRegUser />
+                    </Pic>
+                    <Tittle>
+                        Assigned to me
+                    </Tittle>
+                </Div>
+                <Div>
+                    <Pic>
+                        <FiHome />
+                    </Pic>
+                    <Tittle>
+                        Tasks
+                    </Tittle>
+                </Div>
+            </Menu>
             <Menu2>
                 <Icon>
                     <a href="https://outlook.live.com/mail/"><MdOutlineEmail /></a>
@@ -27,7 +74,7 @@ const Sidebar = () => {
                     <a href="https://outlook.live.com/files/"><MdOutlineAttachFile /></a>
                 </Icon>
                 <Icon>
-                    <a href="https://to-do.live.com/tasks/"><TiTickOutline /></a>
+                    <a href="https://to-do.live.com/tasks/"><span><TiTickOutline /></span></a>
                 </Icon>
             </Menu2>
         </Container>
@@ -39,9 +86,9 @@ export default Sidebar;
 
 const Container = styled.div`
     width: 300px;
-    background-color: black;
+    background-color: white;
     height: calc(100vh - 55px);
-    box-shadow: rgba(0, 0, 0, 0.05) 0px 1px 2px 0px;
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
     display: flex;
     align-items: center;
     justify-content: space-between;
@@ -49,12 +96,11 @@ const Container = styled.div`
 `;
 const Menu = styled.div`
     width: 100%;
-    background-color: red;
+    /* background-color: red; */
     padding: 10px 0px 10px 0px;
 `;
 const Menu2 = styled.div`
     width: 100%;
-    background-color: orange;
     padding: 10px 0px 0px 0px;
     display: flex;
     align-items: center;
@@ -68,7 +114,54 @@ const Icon = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    :hover{
-        background-color: #FAF9F8;
+    a{
+        color: black;
+        font-size: 20px;
+        span{
+            color: blue;
+        }
     }
+    :hover{
+        background-color: #dfdddb;
+    }
+`;
+const Div = styled.div`
+    width: 100%;
+    padding: 5px 0px 5px 0px;
+    /* background-color: green; */
+    margin-top: 5px;
+    margin-bottom: 5px;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    cursor: pointer;
+    transition: all 350ms;
+    :hover{
+        background-color: skyblue;
+    }
+`;
+const Pic = styled.div`
+    width: 30px;
+    height: 30px;
+    /* background-color: azure; */
+    object-fit: contain;
+    font-size: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+const Tittle = styled.div`
+    width: 200px;
+    height: 30px;
+    /* background-color: brown; */
+    display: flex;
+    align-items: center;
+    span{
+        font-weight: bold;
+    }
+`;
+const Div1 = styled.div`
+    margin-left: 20px;
+    padding: 10px 0px 10px 0px;
+    cursor: pointer;
 `;
